ffb3e9cd11811788c6adc6acbab4eac759d518b8
PortalGolem <portalgolemmail@gmail.com>
2026-08-23 11:11:59 -0700

    Fixed Error

--- Scenes/cylender_contents.gd
+++ Scenes/cylender_contents.gd
@@ -3,9 +3,10 @@ extends Node
 @export var contents:Node3D
 
 var hasContents = true
+var Ve
 
 func _on_cylender_inspect_object() -> void:
 	if hasContents:
 		contents.process_mode = Node.PROCESS_MODE_INHERIT
 		contents.reparent(get_parent(), true)
-		contents.position = position
+		contents.position = get_parent().position
