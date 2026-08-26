2cad18602729e18106e92740816c5e29e1dd3e20
PortalGolem <portalgolemmail@gmail.com>
2026-08-26 07:27:52 -0700

    Camera wiggle can now be defined by direction

--- Scenes/CameraWiggle.gd
+++ Scenes/CameraWiggle.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
+@export var wiggleCenter := Vector2.ZERO
 @export var upWiggleFactor := 1.0
-@export var sideWiggleFactor := 1.0
+@export var leftWiggleFactor := 1.0
+@export var rightWiggleFactor := 1.0
+@export var downWiggleFactor := 1.0
 var currentWiggle := Vector2.ZERO
 var startingRotation:Basis
 
@@ -12,11 +15,21 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var mouse = event.position
 		var screenSize := get_viewport().get_visible_rect().size
-		currentWiggle = -Vector2((mouse.x / screenSize.x - .5) * sideWiggleFactor, (mouse.y / screenSize.y - .5) * upWiggleFactor)
+		currentWiggle = -Vector2(mouse.y / screenSize.y, mouse.x / screenSize.x)
+		currentWiggle.x += wiggleCenter.y
+		currentWiggle.y += wiggleCenter.x
+		if currentWiggle.x > 0:
+			currentWiggle.x *= upWiggleFactor
+		else:
+			currentWiggle.x *= downWiggleFactor
+		if currentWiggle.y > 0:
+			currentWiggle.y *= rightWiggleFactor
+		else:
+			currentWiggle.y *= leftWiggleFactor
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	basis = startingRotation
-	rotate_x(currentWiggle.y)
-	rotate_y(currentWiggle.x)
+	rotate_x(currentWiggle.x)
+	rotate_y(currentWiggle.y)
 	
