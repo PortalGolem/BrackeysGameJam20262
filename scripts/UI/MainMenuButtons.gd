19d2fdfda473c18a6a1e3562ceaf041bc6939609
shachisaretai <anshkathiria@gmail.com>
2026-08-29 02:05:55 -0700

    started implementation of 3d room menu

--- scripts/UI/MainMenuButtons.gd
+++ scripts/UI/MainMenuButtons.gd
@@ -3,11 +3,12 @@ extends Control
 var selection = 1
 var selection_past = 1
 var font = global.Main_Font
-var music = "res://Sounds/Music/ShittyBeachMusic.mp3"
+var music = "res://Sounds/Music/spheretypebeat.mp3"
 var move_sfx = [preload("res://Sounds/Menu/Move/snd_menu_moveA.mp3"), preload("res://Sounds/Menu/Move/snd_menu_moveC1.mp3"),
 preload("res://Sounds/Menu/Move/snd_menu_moveC.mp3"),preload("res://Sounds/Menu/Move/snd_menu_moveD.mp3"),
 preload("res://Sounds/Menu/Move/snd_menu_moveE.mp3"),preload("res://Sounds/Menu/Move/snd_menu_moveG.mp3")]
 var annoying_check = 0
+var main_menu = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
@@ -17,42 +18,50 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-#keep track of what we have selected
-	if Input.is_action_just_pressed("ui_down") == true:
-		selection += 1
-	if Input.is_action_just_pressed("ui_up") == true:
-		selection -= 1
-	if selection > 3:
-		selection = 3
-	if selection < 1:
-		selection = 1
-#play sound effects
-	if selection_past != selection:
-		selection_past = selection
-		global.sound_play($"../AudioStreamPlayer", global.gen_random_choice(move_sfx), 1)
-	set_selection()
+func _physics_process(delta: float) -> void:
+	#font shit
+	add_theme_font_override("normal_font", load(global.Main_Font))
 	
-	if Input.is_action_just_pressed("ui_confirm") == true:
-		if selection == 1:
-			get_tree().change_scene_to_file("res://Scenes/pop_up.tscn")
-		if selection == 2:
-			get_tree().change_scene_to_file("res://SettingsMenu.tscn")
-		if selection == 3:
-			get_tree().quit()
+	#check if we back to the main men
+	if !main_menu:
+		self.visible = false
+		if !$"../SettingsMenu".settings:
+			main_menu = true
 	
-	
-	
-	#set up music player and add music
-	if annoying_check == 0:
-		if global.Music == null:
-			const Music_Scene = preload("res://Scenes/music_stream_player.tscn")
-			var Music_Player = Music_Scene.instantiate()
-			get_tree().root.add_child(Music_Player)
-			global.Music = "res://Sounds/Music/ShittyBeachMusic.mp3"
-			annoying_check += 1
-	
-	
+	#only display if we true
+	else:
+		self.visible = true
+	#keep track of what we have selected
+		if Input.is_action_just_pressed("ui_down") == true:
+			selection += 1
+		if Input.is_action_just_pressed("ui_up") == true:
+			selection -= 1
+		if selection > 3:
+			selection = 3
+		if selection < 1:
+			selection = 1
+	#play sound effects
+		if selection_past != selection:
+			selection_past = selection
+			global.sound_play($"../AudioStreamPlayer", global.gen_random_choice(move_sfx), 1)
+		set_selection()
+		if Input.is_action_just_pressed("ui_confirm") == true:
+			if selection == 1:
+				get_tree().change_scene_to_file("res://Scenes/TableSpace.tscn")
+			if selection == 2:
+				$"../SettingsMenu".settings = true
+				main_menu = false
+			if selection == 3:
+				get_tree().quit()
+		
+		#set up music player and add music
+		if annoying_check == 0:
+			if global.Music == null:
+				const Music_Scene = preload("res://Scenes/music_stream_player.tscn")
+				var Music_Player = Music_Scene.instantiate()
+				get_tree().root.add_child(Music_Player)
+				global.Music = "res://Sounds/Music/spheretypebeat.mp3"
+				annoying_check += 1
 	pass
 
 func set_selection():
