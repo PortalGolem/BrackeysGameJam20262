c01e00ef6c3b40f1d992df3af824cd9c4901b47f
PortalGolem <portalgolemmail@gmail.com>
2026-08-29 17:30:34 -0700

    Added wind effects

--- scripts/Items/Dropable/ventricle.gd
+++ scripts/Items/Dropable/ventricle.gd
@@ -3,8 +3,26 @@ signal submitItem(object:Node3D, ventricle:int)
 @export var ventricleID := 0
 
 var sound_open = preload("res://Sounds/Ventitties/venboobyopen/venboobyopen.wav")
+@onready var ventricleVisual := $VentricleHover
 func _on_dropable_trigger_dropped_function(object:Node3D) -> void:
 	submitItem.emit(object,  ventricleID)
 	$AudioStreamPlayer3D.stream = sound_open
 	$AudioStreamPlayer3D.play()
 	object.queue_free()
+
+
+
+func _on_dropable_body_entered(body: Node3D) -> void:
+	if body is Item:
+			ventricleVisual.visible = true
+	for node in body.get_children():
+		if node is Item:
+			ventricleVisual.visible = true
+
+
+func _on_dropable_body_exited(body: Node3D) -> void:
+	if body is Item:
+			ventricleVisual.visible = false
+	for node in body.get_children():
+		if node is Item:
+			ventricleVisual.visible = false
